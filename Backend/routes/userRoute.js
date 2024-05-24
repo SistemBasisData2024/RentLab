378fa0205001552ff1d844a86240e93352cb2fa3
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2024-05-24 22:05:48 +0700

    Update userRoute.js

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -4,5 +4,7 @@ const userRouter = express.Router();
 
 userRouter.post("/signup", userController.signup);
 userRouter.post("/login", userController.login);
+userRouter.post("/signupaslab", userController.signupaslab);
+userRouter.post("/loginaslab", userController.loginaslab);
 
 module.exports = userRouter;
