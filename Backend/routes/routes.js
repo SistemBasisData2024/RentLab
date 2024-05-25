007e416d6d7dfa8d6710b4c43c71b8255ab76da0
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2024-05-25 07:27:54 +0700

    Delete Backend directory

--- Backend/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userRouter = require("./userRoute.js");
-
-const router = express.Router();
-
-router.use("/user", userRouter);
-
-module.exports = router;
