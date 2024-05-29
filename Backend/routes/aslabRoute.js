0f69bfd16e66a9d684ba976b9328f13fbb4dceb4
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2024-05-29 14:33:09 +0700

    Delete Backend directory

--- Backend/routes/aslabRoute.js
+++
@@ -1,9 +0,0 @@
-const aslabController = require("../controllers/aslabController");
-const express = require("express");
-const aslabRouter = express.Router();
-
-aslabRouter.post("/signup", aslabController.signupaslab);
-aslabRouter.post("/login", aslabController.loginaslab);
-aslabRouter.get("/getAslabById/:id", aslabController.getAslabById);
-
-module.exports = aslabRouter;
--- Backend/routes/labRoute.js
+++
@@ -1,7 +0,0 @@
-const labController = require("../controllers/labController");
-const express = require("express");
-const labRouter = express.Router();
-
-labRouter.get("/getAll", labController.getAllLab);
-
-module.exports = labRouter;
